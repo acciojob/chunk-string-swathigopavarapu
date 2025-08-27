ebdc7a085b7333036a1e8459b5b999ef5e1dc08f
swathigopavarapu <41499776+swathigopavarapu@users.noreply.github.com>
2025-08-27 21:33:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function stringChop(str, size) {
   // your code here
+	if (!str) return [];
+	if (size <= 0) return [str];
+	let result = [];
+	for (let i = 0; i < str.length; i += size) {
+		result.push(str.substring(i, i + size));
+	}
+	return result;
+		
+
 }
 
 // Do not change the code below
